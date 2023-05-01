70f9011c9ef2e9ad29fd95d9d59fb73b8e26b6dd
hedleydarsh <hedley.ti@gmail.com>
2023-05-01 18:16:35 -0300

    refactor: :recycle: removendo o composer require

--- dev-setup.sh
+++ dev-setup.sh
@@ -3,5 +3,4 @@ docker run --rm \
     -v "$(pwd):/var/www/html" \
     -w /var/www/html \
     laravelsail/php82-composer:latest \
-    composer require pestphp/pest-plugin-laravel
     composer install --ignore-platform-reqs
